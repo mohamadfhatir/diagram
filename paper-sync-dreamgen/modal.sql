054eaff6d460f5563024d19b005a0d1a41a4e42b
Mohamad Fhatir <136327371+mohamadfhatir@users.noreply.github.com>
2025-10-21 15:24:39 +0700

    feat: add arango and rename hte table name

--- paper-sync-dreamgen/modal.sql
+++ paper-sync-dreamgen/modal.sql
@@ -1,10 +1,11 @@
-CREATE TABLE paper_sync_diff_snapshot (
+
+CREATE TABLE snapshot (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     collection_name VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE paper_sync_schema_collection_log (
+CREATE TABLE mysql_schema_collection_log (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     snapshot_id BIGINT,
     host VARCHAR(255),
@@ -17,10 +18,10 @@ CREATE TABLE paper_sync_schema_collection_log (
     is_nullable BOOLEAN,
     column_default VARCHAR(255),
     extra_info TEXT,
-    FOREIGN KEY (snapshot_id) REFERENCES paper_sync_diff_snapshot(id)
+    FOREIGN KEY (snapshot_id) REFERENCES snapshot(id)
 );
 
-CREATE TABLE paper_sync_index_collection_log (
+CREATE TABLE mysql_index_collection_log (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     snapshot_id BIGINT,
     host VARCHAR(255),
@@ -32,23 +33,28 @@ CREATE TABLE paper_sync_index_collection_log (
     column_name VARCHAR(255),
     index_type VARCHAR(255),
     is_unique BOOLEAN,
-    FOREIGN KEY (snapshot_id) REFERENCES paper_sync_diff_snapshot(id)
+    FOREIGN KEY (snapshot_id) REFERENCES snapshot(id)
 );
-
-CREATE TABLE paper_sync_schema_diff (
+CREATE TABLE arango_index_collection_log (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     snapshot_id BIGINT,
-    source_snapshot_id BIGINT,
-    target_snapshot_id BIGINT,
-    diff_json JSON,
-    FOREIGN KEY (snapshot_id) REFERENCES paper_sync_diff_snapshot(id)
+    collection_name VARCHAR(255),
+    index_name VARCHAR(255),
+    index_type VARCHAR(50),
+    fields JSON,
+    is_unique BOOLEAN,
+    is_sparse BOOLEAN,
+    selectivity_estimate FLOAT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (snapshot_id) REFERENCES snapshot(id)
 );
-
-CREATE TABLE paper_sync_index_diff (
+CREATE TABLE arango_collection_collection_log (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     snapshot_id BIGINT,
-    source_snapshot_id BIGINT,
-    target_snapshot_id BIGINT,
-    diff_json JSON,
-    FOREIGN KEY (snapshot_id) REFERENCES paper_sync_diff_snapshot(id)
+    name VARCHAR(255) NOT NULL,
+    status INT,
+    type INT,
+    is_system BOOLEAN,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (snapshot_id) REFERENCES snapshot(id)
 );
